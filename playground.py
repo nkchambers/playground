45eea924d7ab5e455bfd506ae9e08fbc06576497
nkchambers <nkchames@gmail.com>
2023-04-04 22:07:35 -0700

    adding playground for 4-4-23

--- playground.py
+++ playground.py
@@ -4,20 +4,20 @@ app = Flask(__name__)
 
 # Level 1 - when user visits /play have it render three blue boxes
 @app.route('/play')
-def playground_level1():
+def playground_level_one():
     return render_template('playground_index.html', num=3, color='rgb(30, 220, 245)')
 
 
 # Level 2 - when user visits /play/(x) have it render blue boxes x number of times (/play/7) should render 7 blue boxes
 @app.route('/play/<int:num>')
-def playground_level2(num):
+def playground_level_two(num):
     return render_template('playground_index.html', num=num, color='rgb(30, 220, 245)')
 
 
 # Level 3 - when user visits /play/(x)/color have it render boxes x number of times in color selected 
 # (/play_color/10/green) should render 10 green boxes
 @app.route('/play/<int:num>/<string:color>')
-def playground_level3(num, color):
+def playground_level_three(num, color):
     return render_template('playground_index.html', num=num, color=color)
 
 
